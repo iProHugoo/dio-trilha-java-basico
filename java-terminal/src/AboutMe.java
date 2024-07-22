7d9290db2ca9ce2b9a4810ced1db1e21ae9457dc
Hugo Coelho <iProHugoo@outlook.com>
2024-07-22 20:03:18 -0300

    Estruturas Condicionais

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -2,6 +2,8 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
+	private static Scanner scanner;
+
 	public static void main(String[] args) {
 		/*
 		// Os argumentos começam com índice 0
@@ -15,8 +17,7 @@ public class AboutMe {
 		System.out.println("Minha altura é " + altura + "cm ");
 		*/
 		
-		// Criando o objeto scanner
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+		scanner = new Scanner(System.in).useLocale(Locale.US);
         
         System.out.println("Digite seu nome: ");
         String nome = scanner.next();
